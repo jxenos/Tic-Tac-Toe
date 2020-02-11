a16b2f457a95bf6bdb900b47a7ad74ac29caacce
Jorj Xenos <jorjxenos@gmail.com>
2020-02-11 16:55:51 -0500

    start figuring out how to programmatically check for win condition

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,7 +5,8 @@ using UnityEngine.UI;
 
 public class GameController: MonoBehaviour
 {
-    public Text[] buttonList;
+    public Text[] buttonList_raw;
+    public Text[,] buttonList;
     string playerSide;
 
     void Awake() {
@@ -14,9 +15,17 @@ public class GameController: MonoBehaviour
     }
 
     void SetGameControllerReferenceOnButtons() {
-        for(int i = 0; i < buttonList.Length; i++){
-            buttonList[i].GetComponentInParent<GridSpace>().SetGameControllerReference(this);
-            Debug.Log(buttonList[i]);
+        for (int i = 0; i < buttonList.Length; i++)
+        {
+            buttonList_raw[i].GetComponentInParent<GridSpace>().SetGameControllerReference(this);
+
+            for (int x = 0; x < buttonList.Length; x++)
+            {
+                for (int y = 0; y < buttonList.Length; y++)
+                {
+                    buttonList[x][y] = buttonList_raw[i];
+                }
+            }
         }
     }
 
@@ -26,6 +35,20 @@ public class GameController: MonoBehaviour
 
     public void EndTurn () {
         playerSide = GetPlayerSide() == "X" ? "O" : "X";
+        //  check for win
+        //[0,1,2]
+        //[3,4,5]
+        //[6,7,8]
+        private Text[] win_line[];
+
+        for (int x = 0; x < buttonList.Length; x++)
+        {
+            for (int y = 0; y < buttonList.Length; y++)
+
+            {
+                win_line[y] = buttonList_raw[x][y];
+            }
+        }
     }
 
 }
