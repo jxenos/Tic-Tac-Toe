904ae99741b6cb4ed9fbe6a5f8d75db63a6ec4ad
Jorj Xenos <jorjxenos@gmail.com>
2020-02-11 16:08:04 -0500

    new code with bug fix and same git hierarchy

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController: MonoBehaviour
+{
+    public Text[] buttonList;
+    
+    void Awake() {
+        SetGameControllerReferenceOnButtons();
+    }
+
+    void SetGameControllerReferenceOnButtons() {
+        for(int i = 0; i < buttonList.Length; i++){
+            buttonList[i].GetComponentInParent<GridSpace>().SetGameControllerReference(this);
+            Debug.Log(buttonList[i]);
+        }
+    }
+
+    public string GetPlayerSide() {
+        return "?";
+    }
+
+    public void EndTurn () {
+        Debug.Log("EndTurn is not implemented!");
+    }
+
+}
