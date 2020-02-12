e32ae3754ddf6bf9c270feaad5cb5bff1ee9e9ae
Jorj Xenos <jorjxenos@gmail.com>
2020-02-12 17:12:17 -0500

    work on sorting out the win condition

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,8 +5,7 @@ using UnityEngine.UI;
 
 public class GameController: MonoBehaviour
 {
-    public Text[] buttonList_raw;
-    public Text[,] buttonList;
+    public Text[] buttonList;
     string playerSide;
 
     void Awake() {
@@ -17,15 +16,7 @@ public class GameController: MonoBehaviour
     void SetGameControllerReferenceOnButtons() {
         for (int i = 0; i < buttonList.Length; i++)
         {
-            buttonList_raw[i].GetComponentInParent<GridSpace>().SetGameControllerReference(this);
-
-            for (int x = 0; x < buttonList.Length; x++)
-            {
-                for (int y = 0; y < buttonList.Length; y++)
-                {
-                    buttonList[x][y] = buttonList_raw[i];
-                }
-            }
+            buttonList[i].GetComponentInParent<GridSpace>().SetGameControllerReference(this);
         }
     }
 
@@ -39,16 +30,20 @@ public class GameController: MonoBehaviour
         //[0,1,2]
         //[3,4,5]
         //[6,7,8]
-        private Text[] win_line[];
-
-        for (int x = 0; x < buttonList.Length; x++)
+        Debug.Log(buttonList[0].text);
+        if ((buttonList[0].text == buttonList[1].text == buttonList[2].text) ||
+            (buttonList[3].text == buttonList[4].text == buttonList[5].text) ||
+            (buttonList[6].text == buttonList[7].text == buttonList[8].text) ||
+            (buttonList[0].text == buttonList[3].text == buttonList[6].text) ||
+            (buttonList[1].text == buttonList[4].text == buttonList[7].text) ||
+            (buttonList[2].text == buttonList[5].text == buttonList[8].text) ||
+            (buttonList[0].text == buttonList[4].text == buttonList[8].text) ||
+            (buttonList[2].text == buttonList[4].text == buttonList[6].text))
         {
-            for (int y = 0; y < buttonList.Length; y++)
-
-            {
-                win_line[y] = buttonList_raw[x][y];
-            }
+            //win game
+            Debug.Log("win game");
         }
+        
     }
 
 }
